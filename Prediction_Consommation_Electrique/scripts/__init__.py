e4385f353c9eede73234aa219c57f2dff9ebc1fb
OumaimaElAsri <elasriioumaima@gmail.com>
2026-04-12 08:51:00 +0200

    chore: trim package init to core modules only

--- Prediction_Consommation_Electrique/scripts/__init__.py
+++ Prediction_Consommation_Electrique/scripts/__init__.py
@@ -1,9 +1,3 @@
-"""
-Scripts pour le projet de prédiction de consommation électrique
-"""
+"""Modules core du pipeline de prediction."""
 
-from .data_cleaning import load_and_clean_data
-from .data_preprocessing import preprocess_data
-from .utils import logger
-
-__all__ = ['load_and_clean_data', 'preprocess_data', 'logger']
+__all__ = []
